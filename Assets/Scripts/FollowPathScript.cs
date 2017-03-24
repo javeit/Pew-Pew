04ca64402899ad1ef7d88b610f7d88358259739d
scott-ritchie-1394 <cheasescott@gmail.com>
2017-03-23 19:44:53 -0500

    Initial Commit

---
+++ Assets/Scripts/FollowPathScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathScript : MonoBehaviour {
+
+	public float pathTime;
+
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("PlayerPath"), "time", pathTime, "easetype", iTween.EaseType.easeInOutSine, "orientToPath",true, "lookTime", 0.2, "looptype", iTween.LoopType.loop, "delay", 0));
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed;
+
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		float verticalMoveSpeed = maxSpeed * Input.GetAxis ("Vertical");
+		float horizontalMoveSpeed = maxSpeed * Input.GetAxis ("Horizontal");
+
+		if (((transform.localPosition.x < -3.5) && (horizontalMoveSpeed < 0)) || ((transform.localPosition.x > 3.5) && (horizontalMoveSpeed > 0))) {
+			horizontalMoveSpeed = 0;
+		}
+
+		if (((transform.localPosition.y < -2) && (verticalMoveSpeed < 0)) || ((transform.localPosition.y > 2) && (verticalMoveSpeed > 0))) {
+			verticalMoveSpeed = 0;
+		}
+
+
+		rigidBody.MovePosition (transform.position + (transform.up * verticalMoveSpeed * Time.fixedDeltaTime) + (transform.right * horizontalMoveSpeed * Time.fixedDeltaTime));
+	}
+
+	void OnCollisionEnter(Collision col){
+		Destroy (gameObject);
+	}
+}
