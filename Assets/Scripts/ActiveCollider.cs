f6df59d9f4deccbd1b5d067971cca3abd4d5b97a
DCHolland <Dcholla@okstate.edu>
2017-04-12 19:52:25 -0500

    Enemy Prefabs added
    added basic grunt prefab and script

---
+++ Assets/Scripts/ActiveCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ActiveCollider : MonoBehaviour {
+
+	public EnemyBehavior enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+			enemy.Activate ();
+		transform.parent = null;
+	}
+}
