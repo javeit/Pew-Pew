475aa339c26f234918221d7c60e1d2d7c5a5f513
Joshua Veit <javeit@okstate.edu>
2017-04-18 19:18:43 -0500

    Overall update
    I altered the player weaponry and the environment in several ways,
    please check it out if you have the time.

---
+++ Assets/Scripts/Player/LaserScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+	LineRenderer line;
+
+	void Start () {
+		line = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			StopCoroutine ("FireLaser");
+			StartCoroutine ("FireLaser");
+		}
+	}
+
+	IEnumerator FireLaser () {
+		line.enabled = true;
+
+		while (Input.GetButton("Fire1")) {
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+
+			line.SetPosition (0, ray.origin);
+			if (Physics.Raycast (ray, out hit, 100)) {
+				line.SetPosition (1, hit.point);
+				if (hit.rigidbody) {
+					if (hit.rigidbody.gameObject.tag == "Enemy") {
+						Destroy (hit.rigidbody.gameObject);
+					}
+				}
+			} else {
+				line.SetPosition (1, ray.GetPoint (500));
+			}
+
+			yield return null;
+		}
+
+		line.enabled = false;
+	}
+}
