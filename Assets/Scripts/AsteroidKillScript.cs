f83f8a99b7fdefe5e50180690ceba0fc5f11571c
Voxxyn <unconfigured@null.spigotmc.org>
2017-04-27 17:27:57 -0500

    Asteroid Spawner prefab added

---
+++ Assets/Scripts/AsteroidKillScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidKillScript : MonoBehaviour {
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce(Random.Range (-900.0f, 900.0f), Random.Range (-3900.0f, 900.0f), Random.Range (-900.0f, 900.0f));
+		transform.rotation = Random.rotation;
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/AsteroidSpawnScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawnScript : MonoBehaviour {
+
+	//array of objects to spawn
+	public GameObject [] Asteroids;
+
+
+	//many?
+	public bool denseAsteroidField;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GenerateAsteroids ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//coroutine to spawn asteroids
+	IEnumerator GenerateAsteroids() {
+		while (true) {
+			//spawn on Right
+			Vector3 spawnPoint = new Vector3 (70, Random.Range (-10.0f, 10.0f), Random.Range (-10.0f, 10.0f));
+			Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPoint, Quaternion.identity);
+
+			//Spawn of left
+			Vector3 spawnPoint2 = new Vector3 (-70, Random.Range (-10.0f, 10.0f), Random.Range (-10.0f, 10.0f));
+			Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPoint2, Quaternion.identity);
+
+			//spawn on Top
+			Vector3 spawnPointT = new Vector3 (Random.Range (-10.0f, 10.0f), 70, Random.Range (-10.0f, 10.0f));
+			Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPointT, Quaternion.identity);
+
+			//spawn on Bottom
+			Vector3 spawnPointB = new Vector3 (Random.Range(-10.0f, 10.0f), -70, Random.Range (-10.0f, 10.0f));
+			Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPointB, Quaternion.identity);
+
+
+			//if in densefield, spawn moar
+			if(denseAsteroidField){
+				//spawn on Right
+				Vector3 spawnPoint3 = new Vector3 (70, Random.Range (-10.0f, 0f), Random.Range (-10.0f, 10.0f));
+				Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPoint3, Quaternion.identity);
+
+				//Spawn of left
+				Vector3 spawnPoint4 = new Vector3 (-70, Random.Range (-10.0f, 0f), Random.Range (-10.0f, 10.0f));
+				Instantiate (Asteroids [Random.Range (0, Asteroids.Length)], gameObject.transform.position + spawnPoint4, Quaternion.identity);
+				
+			}
+			yield return new WaitForSeconds (1.0f);
+		}
+	}
+}
