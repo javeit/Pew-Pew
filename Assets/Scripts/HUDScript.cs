1949ebec28dd3839220a6c25c06e499debd38618
scott-ritchie-1394 <cheasescott@gmail.com>
2017-04-27 02:12:59 -0500

    Scene change test
    added cube to drydock

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour {
+
+	private GameObject pauseButton;
+	// Use this for initialization
+	void Start () {
+		Button btn = pauseButton.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
