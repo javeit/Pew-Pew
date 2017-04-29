be09ab22be6f11722540d8e3226aa113557b327f
Joshua Veit <javeit@okstate.edu>
2017-04-28 19:47:35 -0500

    Player and path added to level 2
    Still needs the hud to function properly, but I didn't want to mess up
    the hud by trying to add it to the scene.

---
+++ Assets/Scripts/Paths/FollowPathScriptLevel2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathScriptLevel2 : MonoBehaviour {
+
+	public float pathTime;
+
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("PlayerPath"), "time", pathTime, "easetype", iTween.EaseType.linear, "orientToPath",true));
+	}
+}
