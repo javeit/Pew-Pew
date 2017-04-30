0449f5f240f96e663d104b2e1897cf7bf4f6073a
scott-ritchie-1394 <cheasescott@gmail.com>
2017-04-29 21:29:33 -0500

    Menus/ShipSelect
    -Added main menu
    -Added shipselect menu
    -Implemented ship select in test scene

---
+++ Assets/Scripts/HUDScripts/HUDMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUDMainMenu : MonoBehaviour {
+
+	public Button[] buttons;
+	// Use this for initialization
+	void Start () {
+		buttons = this.gameObject.transform.GetChild(0).GetComponentsInChildren<Button>();
+		buttons[0].onClick.AddListener(StartGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void StartGame()
+	{
+		SceneManager.LoadScene ("Ship Select",LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/HUDScripts/HUDShipSelect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUDShipSelect : MonoBehaviour {
+
+	public Button[] buttons;
+	// Use this for initialization
+	void Start () {
+		buttons = this.gameObject.transform.GetChild(0).GetComponentsInChildren<Button>();
+		buttons[0].onClick.AddListener(SetCrystal);
+		buttons[1].onClick.AddListener(SetStarfighter);
+		buttons[2].onClick.AddListener(SetSwordfish);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void SetCrystal()
+	{
+		PlayerPrefs.SetInt("ShipSelect", 0);
+		SceneManager.LoadScene ("Test Scene",LoadSceneMode.Single);
+	}
+	void SetStarfighter()
+	{
+		PlayerPrefs.SetInt("ShipSelect", 1);
+		SceneManager.LoadScene ("Test Scene",LoadSceneMode.Single);
+	}
+	void SetSwordfish()
+	{
+		PlayerPrefs.SetInt("ShipSelect", 2);
+		SceneManager.LoadScene ("Test Scene",LoadSceneMode.Single);
+	}
+}
