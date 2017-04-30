717890f27fcf6d4156322e747584ceb9bea19c1c
Voxxyn <unconfigured@null.spigotmc.org>
2017-04-30 17:51:02 -0500

    Game over man! core now properly calls win game script

--- Assets/Scripts/WinGame.cs
+++ Assets/Scripts/WinGame.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class WinGame : MonoBehaviour {
 
+	public GameObject Core;
+
 	//when capital ship core dies, go to game over screen
-	void OnDestroy() {
-		StartCoroutine (wait5secs ());
+	void Update (){
+		if (!Core) {
+			StartCoroutine(wait5secs());
+		}
 	}
 	IEnumerator wait5secs() {
 		yield return new WaitForSeconds (5);
