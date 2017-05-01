9120d36c713e9471691fd82afd5accf796601d2b
Voxxyn <unconfigured@null.spigotmc.org>
2017-05-01 07:01:25 -0500

    enemyBox tag on colliders

--- Assets/Scripts/EnemyScripts/ActiveCollider.cs
+++ Assets/Scripts/EnemyScripts/ActiveCollider.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class ActiveCollider : MonoBehaviour {
 
+	public int count = 0;
+
+	private int passes = 0;
+
 	public EnemyBehavior enemy;
 	// Use this for initialization
 	void Start () {
@@ -19,8 +23,15 @@ public class ActiveCollider : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.CompareTag("Player"))
+		if (other.CompareTag ("Player") && passes >= count) {
+			passes++;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Player") && passes >= count) {
 			enemy.Activate ();
-		transform.parent = null;
+			transform.parent = null;
+		}
 	}
 }
