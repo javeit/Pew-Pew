1a1b12a2623974b2b032eb305a5d358a6abc58be
javeit <joshuaveit1995@gmail.com>
2017-05-01 16:53:57 -0500

    One more change needs testing

--- Assets/Scripts/Player/LaserScript.cs
+++ Assets/Scripts/Player/LaserScript.cs
@@ -7,22 +7,39 @@ public class LaserScript : MonoBehaviour {
 	LineRenderer line;
 	//Need this to disable while paused -Scott
 	HUDScript hudScript;
+
+	private bool OSX;
 	void Start () {
 		line = GetComponent<LineRenderer> ();
 		hudScript = GameObject.Find("CanvasMain").GetComponent<HUDScript>();
+
+		if (Application.platform == RuntimePlatform.OSXEditor ||
+			Application.platform == RuntimePlatform.OSXDashboardPlayer ||
+			Application.platform == RuntimePlatform.OSXPlayer) {
+			OSX = true;
+		} else {
+			OSX = false;
+		}
 	}
 
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			StopCoroutine ("FireLaser");
-			StartCoroutine ("FireLaser");
+		if (!OSX) {
+			if (Input.GetButtonDown ("Fire1") || Input.GetAxis("Fire1 Windows") > 0f) {
+				StopCoroutine ("FireLaser");
+				StartCoroutine ("FireLaser");
+			}
+		} else {
+			if (Input.GetButtonDown ("Fire1") || Input.GetAxis("Fire1 Mac") > 0f) {
+				StopCoroutine ("FireLaser");
+				StartCoroutine ("FireLaser");
+			}
 		}
 	}
 
 	IEnumerator FireLaser () {
 		line.enabled = true;
 
-		while (Input.GetButton("Fire1") && !hudScript.getPaused()) {
+		while ((Input.GetButtonDown ("Fire1") || (!OSX && Input.GetAxis("Fire1 Windows") > 0f) || (OSX && Input.GetAxis("Fire1 Mac") > 0f)) && !hudScript.getPaused()) {
 			Ray ray = new Ray (transform.position, transform.forward);
 			RaycastHit hit;
 
--- Assets/Scripts/Player/PlayerGunScript.cs
+++ Assets/Scripts/Player/PlayerGunScript.cs
@@ -13,26 +13,51 @@ public class PlayerGunScript : MonoBehaviour {
 	private HUDScript hudScript;
 	private float timeToFire;
 	private AudioSource shotSound;
+	private bool OSX;
 
 	void Start () {
 		timeToFire = 0;
 		shotSound = this.GetComponent<AudioSource>();
 		hudScript = GameObject.Find("CanvasMain").GetComponent<HUDScript>();
+
+		if (Application.platform == RuntimePlatform.OSXEditor ||
+			Application.platform == RuntimePlatform.OSXDashboardPlayer ||
+			Application.platform == RuntimePlatform.OSXPlayer) {
+			OSX = true;
+		} else {
+			OSX = false;
+		}
 	}
 
 	void Update () {
-		if (Input.GetButton ("Fire1") && hudScript.getPaused() == false) {
-			if (timeToFire <= 0) {
-				GameObject newBullet = Instantiate (bulletPrefab, pathObject);
-				shotSound.Play();
-				newBullet.transform.position = barrel.position;
-				newBullet.transform.rotation = Quaternion.LookRotation (transform.forward);
-				timeToFire = timeBetweenShots;
-			} else {
-				timeToFire -= Time.deltaTime;
+		if (!OSX) {
+			if ((Input.GetButton ("Fire1") || Input.GetAxis("Fire1 Windows") > 0f)&& hudScript.getPaused () == false) {
+				if (timeToFire <= 0) {
+					GameObject newBullet = Instantiate (bulletPrefab, pathObject);
+					shotSound.Play ();
+					newBullet.transform.position = barrel.position;
+					newBullet.transform.rotation = Quaternion.LookRotation (transform.forward);
+					timeToFire = timeBetweenShots;
+				} else {
+					timeToFire -= Time.deltaTime;
+				}
+			} else if (timeToFire > 0) {
+				timeToFire = 0;
 			}
-		} else if(timeToFire > 0){
-			timeToFire = 0;
+		} else {
+			if ((Input.GetButton ("Fire1") || Input.GetAxis("Fire1 Windows") > 0f) && hudScript.getPaused () == false) {
+				if (timeToFire <= 0) {
+					GameObject newBullet = Instantiate (bulletPrefab, pathObject);
+					shotSound.Play ();
+					newBullet.transform.position = barrel.position;
+					newBullet.transform.rotation = Quaternion.LookRotation (transform.forward);
+					timeToFire = timeBetweenShots;
+				} else {
+					timeToFire -= Time.deltaTime;
+				}
+			} else if (timeToFire > 0) {
+				timeToFire = 0;
+			}
 		}
 	}
 }
