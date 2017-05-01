d8a43e2cec6e48b52e4f95d4230810a7a7f86e26
scott-ritchie-1394 <cheasescott@gmail.com>
2017-04-30 23:36:56 -0500

    -slider prefab

---
+++ Assets/Scripts/HUDScripts/HealthSliderScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSliderScript : MonoBehaviour {
+
+	public Slider musicSlider;
+	public int health;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		musicSlider.value = health;
+	}
+}
