73ea5d4d3c040f5fa1b00890be2e4cddea99df40
scott-ritchie-1394 <cheasescott@gmail.com>
2017-05-01 03:22:00 -0500

    -started audio work

---
+++ Assets/Scripts/HUDScripts/Dialogue/LVL1DialogueScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LVL1DialogueScript : MonoBehaviour {
+
+	public GameObject sources;
+	public AudioSource[] dialogue;
+	public MoveDialogueScript moveScript;
+	public float[,] delays;
+	public float time = 0;
+	public int count = 0;
+	// Use this for initialization
+	void Start () {
+		dialogue = sources.GetComponents<AudioSource>();
+		moveScript = GameObject.Find("Dialogue").GetComponent<MoveDialogueScript>();
+		delays = new float[dialogue.Length,2];
+		delays[0,0] = 2f;
+		delays[1,0] = dialogue[0].clip.length;
+		delays[2,0] = dialogue[1].clip.length;
+		delays[3,0] = 16f;
+		delays[4,0] = dialogue[3].clip.length;
+		delays[5,0] = dialogue[4].clip.length;
+		delays[6,0] = dialogue[5].clip.length;
+		delays[7,0] = dialogue[6].clip.length;
+	}
+	
+	// Update is called once per frame 
+	void Update () {
+		time += Time.deltaTime;
+		if(count != dialogue.Length  &&  time > delays[count,0])
+		{
+			dialogue[count].Play();
+			count++;
+			time = 0f;
+		}
+	}
+}
---
+++ Assets/Scripts/HUDScripts/Dialogue/MoveDialogueScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDialogueScript : MonoBehaviour {
+
+	// Use this for initialization
+	float t;
+	Vector3 startPosition;
+	Vector3 target;
+	float timeToReachTarget;
+	private float dialogueRightX = 481;
+	private float dialogueLeftX = 338;
+	// Use this for initialization
+	void Start () {
+		startPosition = target = transform.GetComponent<RectTransform>().anchoredPosition;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+     {
+             t += Time.deltaTime/timeToReachTarget;
+             transform.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(startPosition, target, t);
+     }
+     public void SetDialogueOut()
+     {
+            t = 0;
+            startPosition = transform.GetComponent<RectTransform>().anchoredPosition;
+            timeToReachTarget = 4f;
+            target = new Vector3(dialogueRightX,startPosition.y,startPosition.z); 
+     }
+	 public void SetDialogueIn(Vector3 destination, float time)
+     {
+            t = 0;
+            startPosition = transform.GetComponent<RectTransform>().anchoredPosition;
+            timeToReachTarget = 4f;
+            target = new Vector3(dialogueLeftX,startPosition.y,startPosition.z); 
+     }
+}
