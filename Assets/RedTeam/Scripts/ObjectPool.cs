e0a598be0b40b082a69f551a3756d4b075e3f007
Joshua Veit <joshuaveit1995@gmail.com>
2019-03-11 19:34:16 -0500

    Added a generic ObjectPool class
    Add the ObjectPool class which can be easliy extended to support various monobehaviour types. The intention of this is that it could be used to reduce the number of Instantiate/Destroy calls with respect to bullets and enemies

---
+++ Assets/RedTeam/Scripts/ObjectPool.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RedTeam {
+
+    public class ObjectPool<T> : MonoBehaviour where T : MonoBehaviour {
+
+        public T original;
+        public int initialPoolSize;
+
+        List<T> _pooledObjects;
+        List<T> _usedObjects;
+
+        public T GetPooledObject() {
+
+            T poolableObject;
+
+            if (_pooledObjects.Count > 0) {
+
+                poolableObject = _pooledObjects[0];
+                _pooledObjects.RemoveAt(0);
+
+            } else if(original != null) {
+
+                poolableObject = Instantiate<T>(original, transform);
+
+            } else {
+
+                poolableObject = null;
+            }
+
+            _usedObjects.Add(poolableObject);
+
+            return poolableObject;
+        }
+
+        public void ReturnToPool(T pooledObject) {
+
+            if (pooledObject == null)
+                return;
+
+            _pooledObjects.Add(pooledObject);
+            _usedObjects.Remove(pooledObject);
+
+            pooledObject.gameObject.SetActive(false);
+            pooledObject.transform.SetParent(transform);
+        }
+
+        void Awake() {
+
+            _pooledObjects = new List<T>();
+            _usedObjects = new List<T>();
+
+            if (original != null) {
+
+                T newPoolableObject;
+
+                for (int i = 0; i < initialPoolSize; i++) {
+
+                    newPoolableObject = Instantiate<T>(original, transform);
+                    newPoolableObject.gameObject.SetActive(false);
+
+                    _pooledObjects.Add(newPoolableObject);
+                }
+            }
+        }
+    }
+}
